1c1e9ccaacaaec94e123a7c73e39cef7abead95f
Leximiller128 <leximiller128@gmail.com>
2022-11-02 14:10:49 -0600

    thought route

--- routes/api/thoughtRoute.js
+++ routes/api/thoughtRoute.js
@@ -1,3 +1,23 @@
 const router = require("express").Router();
 
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  // updateThought,
+  // deleteThought,
+  // addReaction,
+  // deleteReaction,
+} = require("../../controllers/thought-controller");
+
+router.route("/").get(getThoughts).post(createThought);
+
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .delete(deleteThought)
+  .put(updateThought);
+
+// router.route("/:thoughtId/reactions").post(addReaction).delete(deleteReaction);
+
 module.exports = router;
